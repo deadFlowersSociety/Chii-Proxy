35d27b9af1ca858b3dbc8b07d0e43ccd889e2ef0
Chiijlaw <chiijlaw@berkeley.edu>
2018-10-06 17:06:05 -0700

    Change endpoints. Point to local instance of service.

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require ('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-var compression = require('compression');
+require("newrelic");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+var compression = require("compression");
 
 var server = express();
-server.use(compression())
+server.use(compression());
 server.use(bodyParser.json());
-server.use(express.urlencoded({extended: true}));
-server.use(express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, "./"), { maxAge: "30 days" }));
 server.use(cors());
 
 // Albums & Player
-server.get('/artists/albums/:artistID', (req, res) => {
-  res.redirect('http://52.15.129.193' + req.url);
+server.get("/artists/albums/:artistID", (req, res) => {
+  res.redirect("http://52.15.129.193" + req.url);
 });
 
 // Related Artists
-server.get('/artists/:id/relatedArtists', (req, res) => {
-  res.redirect('http://18.206.245.56' + req.url);
+server.get("/artists/:id/relatedArtists", (req, res) => {
+  res.redirect("http://18.206.245.56" + req.url);
 });
 
 // Popular Songs
-server.get('/artists/:id', (req, res) => {
-  res.redirect('http://18.224.17.253' + req.url);
+server.get("/artists/:id", (req, res) => {
+  res.redirect("http://18.224.17.253" + req.url);
 });
 
 // Header
-server.get('/artists/:artistID', (req, res) => {
-   res.redirect('http://35.172.133.115' + req.url);
+server.get("/api/artists/header/:artistID", (req, res) => {
+  res.redirect("http://localhost:3004" + req.url);
 });
 
-server.listen(3000, console.log('Listening on:', 3000));
+server.listen(3000, console.log("Listening on:", 3000));
